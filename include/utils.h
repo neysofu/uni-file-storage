290972cf225290f55736ab66f9c50f5b9de9139d
Filippo Costa @neysofu <filippo@neysofu.me>
2021-05-24 00:51:02 +0200

    docs: Add some doc comments

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,11 @@
 #ifndef SOL_UTILS
 #define SOL_UTILS
 
+/* Suppresses "unused variable" warnings by the compiler. */
 #define UNUSED(x) (void)(x)
 
+/* Suspends the execution of the current thread for approximately `msec`
+ * milliseconds. */
 void
 wait_msec(int msec);
 
