1a3a0fd4fa07f567472742e1f8494f065fd75eeb
Filippo Costa <filippocosta.italy@gmail.com>
2021-05-26 22:53:12 +0200

    chore: Fix .gitignore issue

--- src/server/deserializer.h
+++ src/server/deserializer.h
@@ -6,13 +6,6 @@
 
 struct Deserializer;
 
-// struct Deserializer
-//{
-//	void *buffer;
-//	size_t size_in_bytes;
-//	size_t nominal_size_in_bytes;
-//};
-
 struct Message
 {
 	size_t size_in_bytes;
@@ -21,21 +14,23 @@ struct Message
 	struct ActionArgs args;
 };
 
+/* Allocates a new `struct Deserializer` on the heap with an empty state. Returns
+ * NULL on allocation failure. */
 struct Deserializer *
 deserializer_create(void);
-
-void
-deserializer_clean(struct Deserializer *deserializer);
 
 int
 deserializer_last_message(struct Deserializer *deserializer);
 
+/* Returns a low bound on the number of missing bytes. */
 size_t
 deserializer_missing(const struct Deserializer *deserializer);
 
+/* Returns a pointer to the next, uninitializer portion of its internal buffer. */
 void *
 deserializer_buffer(struct Deserializer *deserializer);
 
+/* Frees all memory and the internal buffer of `deserializer`. */
 void
 deserializer_free(struct Deserializer *deserializer);
 
