920fb2d1fd1fb564f798e5fb82e6cd5156519380
Filippo Costa @neysofu <filippo@neysofu.me>
2021-05-29 03:59:01 +0200

    docs: Add documentation to shutdown functions

--- src/server/shutdown.h
+++ src/server/shutdown.h
@@ -3,15 +3,20 @@
 
 #include <stdbool.h>
 
+/* Trigger a "soft" shutdown, i.e. stop listening for new connections. */
 int
 shutdown_soft(void);
 
+/* Trigger a "hard" shutdown, i.e. stop listening for new connections and close
+ * all existing connections. */
 int
 shutdown_hard(void);
 
+/* Has a soft shutdown occured already? */
 bool
 detect_shutdown_soft(void);
 
+/* Has a hard shutdown occured already? */
 bool
 detect_shutdown_hard(void);
 
