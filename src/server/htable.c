039197cdd08b1625f2f90325d0c0dccda00ce958
Filippo Costa <filippocosta.italy@gmail.com>
2021-06-01 01:51:40 +0200

    refactor: Make the cache API much more elegant and simpler

--- src/server/htable.c
+++ src/server/htable.c
@@ -7,12 +7,12 @@
 #include <string.h>
 
 #define XXHASH_SEED 0
-#define GROWTH_FACTOR 2
 
 struct HTableLlNode
 {
-	struct HTableItem item;
+	struct File item;
 	struct HTableLlNode *next;
+	struct HTableLlNode *prev;
 };
 
 struct HTableBucket
@@ -24,20 +24,35 @@ struct HTableBucket
 
 struct HTable
 {
+	enum CacheReplacementPolicy policy;
+	pthread_mutex_t stats_guard;
 	size_t items_count;
+	size_t max_items_count;
 	size_t buckets_count;
+	size_t total_space_in_bytes;
+	size_t max_space_in_bytes;
 	struct HTableBucket *buckets;
 };
 
 struct HTable *
-htable_new(size_t buckets)
+htable_new(size_t buckets, enum CacheReplacementPolicy policy)
 {
 	assert(buckets > 0);
 	struct HTable *htable = malloc(sizeof(struct HTable));
 	if (!htable) {
 		return NULL;
 	}
+	// FIXME: max limits
+	htable->policy = policy;
+	int err = pthread_mutex_init(&htable->stats_guard, NULL);
+	if (err) {
+		free(htable);
+		return NULL;
+	}
 	htable->items_count = 0;
+	htable->max_items_count = 0;
+	htable->total_space_in_bytes = 0;
+	htable->max_space_in_bytes = 0;
 	htable->buckets_count = buckets;
 	htable->buckets = malloc(sizeof(struct HTableLlNode) * buckets);
 	if (!htable->buckets) {
@@ -47,6 +62,12 @@ htable_new(size_t buckets)
 	for (size_t i = 0; i < buckets; i++) {
 		htable->buckets[i].head = NULL;
 		htable->buckets[i].last = NULL;
+		err = pthread_mutex_init(&htable->buckets[i].guard, NULL);
+		if (err != 0) {
+			free(htable->buckets);
+			free(htable);
+			return NULL;
+		}
 	}
 	return htable;
 }
@@ -57,72 +78,189 @@ htable_free(struct HTable *htable)
 	if (!htable) {
 		return;
 	}
+	for (size_t i = 0; i < htable->buckets_count; i++) {
+		/* Ignore any errors: everything will be out of scope very soon, we don't
+		 * care. */
+		pthread_mutex_destroy(&htable->buckets[i].guard);
+		// TODO: free other stuff
+	}
 	free(htable->buckets);
 	free(htable);
 }
 
-struct HTableItem *
-htable_get(const struct HTable *htable, const char *key)
+struct HTableLlNode *
+htable_fetch_node(struct HTable *htable, const char *key)
 {
+	assert(htable);
+	assert(key);
+	int err = 0;
 	uint64_t hash = XXH64(key, strlen(key), XXHASH_SEED);
 	size_t bucket_i = hash % htable->buckets_count;
-	struct HTableLlNode *node = htable->buckets[bucket_i].head;
+	struct HTableBucket *bucket = &htable->buckets[bucket_i];
+	err = pthread_mutex_lock(&bucket->guard);
+	if (err) {
+		return NULL;
+	}
+	struct HTableLlNode *node = bucket->head;
 	while (node) {
 		if (node->item.key == key) {
-			return &node->item;
+			return node;
 		}
 		node = node->next;
 	}
 	return NULL;
 }
 
-struct HTableItem *
-htable_insert(struct HTable *htable, const char *key)
+struct File *
+htable_fetch_file(struct HTable *htable, const char *key)
 {
-	struct HTableItem *found = htable_get(htable, key);
-	if (found) {
-		return found;
-	}
-	uint64_t hash = XXH64(key, strlen(key), XXHASH_SEED);
-	size_t bucket_i = hash % htable->buckets_count;
-	struct HTableBucket *bucket = &htable->buckets[bucket_i];
-	struct HTableLlNode *node = malloc(sizeof(struct HTableLlNode));
+	struct HTableLlNode *node = htable_fetch_node(htable, key);
 	if (!node) {
 		return NULL;
 	}
-	htable->items_count++;
-	node->next = NULL;
-	bucket->last = node;
-	if (!bucket->head) {
-		bucket->head = node;
-	}
-	return &(node->item);
+	return &node->item;
 }
 
-struct HTable *
-htable_clone(const struct HTable *htable)
+int
+htable_release(struct HTable *htable, const char *key)
 {
-	// Let's keep the load fuctor under 1.0. We technically can tolerate more as
-	// we're using chaining rather than open addressing, but it's a good
-	// default.
-	if (htable->items_count <= htable->buckets_count) {
-		return NULL;
+	uint64_t hash = XXH64(key, strlen(key), XXHASH_SEED);
+	size_t bucket_i = hash % htable->buckets_count;
+	struct HTableBucket *bucket = &htable->buckets[bucket_i];
+	return pthread_mutex_unlock(&bucket->guard);
+}
+
+int
+htable_lock_file(struct HTable *htable, const char *key)
+{
+	struct File *file = htable_fetch_file(htable, key);
+	if (!file) {
+		return -1;
 	}
-	struct HTable *new = htable_new(htable->buckets_count * GROWTH_FACTOR);
-	if (!new) {
-		return NULL;
+	file->is_locked = true;
+	return htable_release(htable, key);
+}
+
+int
+htable_unlock_file(struct HTable *htable, const char *key)
+{
+	struct File *file = htable_fetch_file(htable, key);
+	if (!file) {
+		return -1;
 	}
-	for (size_t i = 0; i < htable->buckets_count; i++) {
-		struct HTableBucket *bucket = &htable->buckets[i];
-		struct HTableLlNode *node = bucket->head;
-		while (node) {
-			struct HTableItem *item = htable_insert(new, node->item.key);
-			item->key = node->item.key;
-			item->value = node->item.value;
-			node = node->next;
-		}
+	file->is_locked = false;
+	return htable_release(htable, key);
+}
+
+int
+htable_open_file(struct HTable *htable, const char *key)
+{
+	struct File *file = htable_fetch_file(htable, key);
+	if (!file) {
+		return -1;
 	}
-	return new;
+	file->is_open = true;
+	return htable_release(htable, key);
+}
+
+/* Runs the cache replacement policy algorithm on `htable` after some operation
+ * that might trigger evictions. `new_file` is `true` iff a new file has just
+ * been created, `new_space_in_bytes` is the new amount of space in bytes that
+ * must be taken into account. `evicted` and `evicted_count` will -after this
+ * call- hold data about evicted files. */
+int
+htable_evict_files(struct HTable *htable,
+                   bool new_file,
+                   size_t new_space_in_bytes,
+                   struct File **evicted,
+                   unsigned *evicted_count)
+{
+	int err = 0;
+	err |= pthread_mutex_lock(&htable->stats_guard);
+	if (new_file) {
+		htable->items_count++;
+	}
+	htable->total_space_in_bytes += new_space_in_bytes;
+	// TODO: evict files...
+	err |= pthread_mutex_unlock(&htable->stats_guard);
+	if (err) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+int
+htable_remove_file(struct HTable *htable, const char *key)
+{
+	struct HTableLlNode *node = htable_fetch_node(htable, key);
+	if (!node) {
+		return -1;
+	}
+	/* Chain together the previous and next nodes within the bucket's linked
+	 * list. */
+	if (node->prev) {
+		node->prev->next = node->next;
+	}
+	if (node->next) {
+		node->next->prev = node->prev;
+	}
+	/* Free stuff. */
+	free(node->item.key);
+	free(node->item.contents);
+	free(node);
+	return htable_release(htable, key);
+}
+
+int
+htable_write_file_contents(struct HTable *htable,
+                           const char *key,
+                           const void *contents,
+                           size_t size_in_bytes,
+                           struct File **evicted,
+                           unsigned *evicted_count)
+{
+	struct File *file = htable_fetch_file(htable, key);
+	if (!file) {
+		return -1;
+	}
+	file->length_in_bytes = size_in_bytes;
+	free(file->contents);
+	file->contents = malloc(size_in_bytes);
+	// TODO check malloc
+	memcpy(file->contents, contents, size_in_bytes);
+	file->length_in_bytes = size_in_bytes;
+	int err = htable_release(htable, key);
+	if (err) {
+		return -1;
+	}
+	return htable_evict_files(htable, true, size_in_bytes, evicted, evicted_count);
+}
+
+int
+htable_append_to_file_contents(struct HTable *htable,
+                               const char *key,
+                               const void *contents,
+                               size_t size_in_bytes,
+                               struct File **evicted,
+                               unsigned *evicted_count)
+{
+	struct File *file = htable_fetch_file(htable, key);
+	if (!file) {
+		return -1;
+	}
+	file->length_in_bytes += size_in_bytes;
+	void *new_buffer = realloc(file->contents, file->length_in_bytes);
+	// TODO malloc check
+	file->contents = new_buffer;
+	memcpy((char *)(new_buffer) + file->length_in_bytes - size_in_bytes,
+	       contents,
+	       size_in_bytes);
+	int err = htable_release(htable, key);
+	if (err) {
+		return -1;
+	}
+	return htable_evict_files(htable, false, size_in_bytes, evicted, evicted_count);
 }
 
 int
--- src/server/htable.h
+++ src/server/htable.h
@@ -1,41 +1,92 @@
 #ifndef SOL_SERVER_HTABLE
 #define SOL_SERVER_HTABLE
 
+#include <stdbool.h>
 #include <stdlib.h>
+
+// TODO: implement these
+enum CacheReplacementPolicy {
+	POLICY_BUCKET_BASED,
+	POLICY_FIFO,
+};
 
 /* A specialized, concurrent hash table for the file storage server. */
 struct HTable;
 
-/* File data. */
+/* File data within a `struct HTable`. */
 struct File
 {
-	void *contents;
-	size_t contents_length_in_bytes;
-	int flags;
-};
-
-/* The contents of an item within a `struct HTable`. */
-struct HTableItem
-{
 	char *key;
-	struct File value;
+	void *contents;
+	size_t length_in_bytes;
+	int fd_owner;
+	bool is_open;
+	bool is_locked;
 };
 
-/* Creates an empty `struct HTable`. */
-struct HTable *
-htable_new(size_t buckets);
+struct HTableVisitor;
 
-/* Deletes `htable` and all its contents from memory (files, flags, etc.). */
+/* Creates an empty `struct HTable` with a fixed number of `buckets`. Returns
+ * NULL on allocation failure or other system errors that make the operation
+ * impossible. */
+struct HTable *
+htable_new(size_t buckets, enum CacheReplacementPolicy policy);
+
+/* Deletes `htable` and all its contents from memory (files, paths, flags, etc.). */
 void
 htable_free(struct HTable *htable);
 
-struct HTableItem *
-htable_get(const struct HTable *htable, const char *key);
-
-struct HTableItem *
-htable_insert(struct HTable *htable, const char *key);
+/* Searches a file with path `key` within `htable` and returns a pointer to its
+ * contents if found, NULL otherwise. */
+struct File *
+htable_fetch_file(struct HTable *htable, const char *key);
 
 int
-htable_remove(struct HTable *htable, const char *key);
+htable_write_file_contents(struct HTable *htable,
+                           const char *key,
+                           const void *contents,
+                           size_t size_in_bytes,
+                           struct File **evicted,
+                           unsigned *evicted_count);
+
+int
+htable_append_to_file_contents(struct HTable *htable,
+                               const char *key,
+                               const void *contents,
+                               size_t size_in_bytes,
+                               struct File **evicted,
+                               unsigned *evicted_count);
+
+/* Drops a unique handle on the file with path `key` within `htable` and releases
+ * an internal lock that blocks access to it from other threads. Returns 0 on
+ * success, -1 on failure. */
+int
+htable_release(struct HTable *htable, const char *key);
+
+/* Locks the file with path `key` within `htable` and returns 0 if the operation
+ * was successful, -1 otherwise. */
+int
+htable_lock_file(struct HTable *htable, const char *key);
+
+/* Unlocks the file with path `key` within `htable` and returns 0 if the operation
+ * was successful, -1 otherwise. */
+int
+htable_unlock_file(struct HTable *htable, const char *key);
+
+/* Opens the file with path `key` within `htable` and returns 0 if the operation
+ * was successful, -1 otherwise. */
+int
+htable_open_file(struct HTable *htable, const char *key);
+
+/* Removes the file with path `key` within `htable` and returns 0 if the operation
+ * was successful, -1 otherwise. */
+int
+htable_remove_file(struct HTable *htable, const char *key);
+
+struct HTableVisitor *
+htable_visit(const struct HTable *htable, unsigned max_visits);
+
+const struct File *
+htable_visitor_next(const struct HTableVisitor *visitor);
 
 #endif
