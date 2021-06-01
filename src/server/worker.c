116da8b8c6d8cd400d56cee1652854d42404a977
Filippo Costa <filippocosta.italy@gmail.com>
2021-06-01 02:12:53 +0200

    feat: Make progress in protocol response

--- src/server/worker.c
+++ src/server/worker.c
@@ -1,4 +1,5 @@
 #include "worker.h"
+#include "htable.h"
 #include "global_state.h"
 #include "logc/src/log.h"
 #include "serverapi_utils.h"
@@ -12,23 +13,28 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+/* Worker ID tracker. */
 struct Worker
 {
-	unsigned worker_id;
+	unsigned id;
 };
 
 void
 worker_handle_read(struct Worker *worker, int fd, void *buffer, size_t len_in_bytes)
 {
-	log_info("New API request `readFile`.");
-	char response[1] = { RESPONSE_OK };
-	writen(fd, response, 1);
+	log_info("[Worker n. %u] New API request `readFile`.", worker->id);
+	struct File *file = htable_fetch_file(htable, buffer);
+	char response[1+8] = { RESPONSE_OK };
+	u64_to_big_endian(file->length_in_bytes, &response[1]);
+	writen(fd, response, 9);
+	writen(fd, file->contents, file->length_in_bytes);
+	htable_release(htable, buffer);
 }
 
 void
 worker_handle_write_file(struct Worker *worker, int fd, void *buffer, size_t len_in_bytes)
 {
-	log_info("New API request `writeFile`.");
+	log_info("[Worker n. %u] New API request `writeFile`.", worker->id);
 	char response[1] = { RESPONSE_OK };
 	writen(fd, response, 1);
 }
@@ -36,25 +42,28 @@ worker_handle_write_file(struct Worker *worker, int fd, void *buffer, size_t len
 void
 worker_handle_lock(struct Worker *worker, int fd, void *buffer, size_t len_in_bytes)
 {
-	log_info("New API request `lockFile`.");
+	log_info("[Worker n. %u] New API request `lockFile`.", worker->id);
+	htable_lock_file(htable, buffer);
 	char response[1] = { RESPONSE_OK };
-	writen(fd, response, 1);
+	int err = write(fd, response, 1);
 }
 
 void
 worker_handle_unlock(struct Worker *worker, int fd, void *buffer, size_t len_in_bytes)
 {
-	log_info("New API request `unlockFile`.");
+	log_info("[Worker n. %u] New API request `unlockFile`.", worker->id);
+	htable_unlock_file(htable, buffer);
 	char response[1] = { RESPONSE_OK };
-	writen(fd, response, 1);
+	int err = write(fd, response, 1);
 }
 
 void
 worker_handle_remove(struct Worker *worker, int fd, void *buffer, size_t len_in_bytes)
 {
-	log_info("New API request `removeFile`.");
+	log_info("[Worker n. %u] New API request `removeFile`.", worker->id);
+	htable_remove_file(htable, buffer);
 	char response[1] = { RESPONSE_OK };
-	writen(fd, response, 1);
+	int err = write(fd, response, 1);
 }
 
 void
@@ -66,7 +75,7 @@ worker_handle_message(struct Worker *worker, int fd, void *buffer, size_t len_in
 		return;
 	}
 	log_debug("[Worker n. %u] The latest message is %zu bytes long.",
-	          worker->worker_id,
+	          worker->id,
 	          len_in_bytes);
 	switch (((char *)(buffer))[8]) {
 		case API_OP_READ_FILE:
@@ -93,15 +102,15 @@ void *
 worker_entry_point(void *args)
 {
 	UNUSED(args);
-	unsigned worker_id = ts_counter();
+	unsigned id = ts_counter();
 	struct Worker worker;
-	worker.worker_id = worker_id;
-	sem_t *sem = &workload_queue_get(worker_id)->sem;
+	worker.id = id;
+	sem_t *sem = &workload_queue_get(id)->sem;
 	while (1) {
 		sem_wait(sem);
-		log_debug("[Worker n. %u] New message incoming. Pulling...", worker_id);
-		struct Message *msg = workload_queue_pull(worker_id);
-		log_debug("[Worker n. %u] Done.", worker_id);
+		log_debug("[Worker n. %u] New message incoming. Pulling...", id);
+		struct Message *msg = workload_queue_pull(id);
+		log_debug("[Worker n. %u] Done.", id);
 		worker_handle_message(&worker, msg->fd, msg->buffer.raw, msg->buffer.size_in_bytes);
 	}
 	return NULL;
