d7dc258a01a8f8bd91166199cf6a554c531373a5
Filippo Costa <filippocosta.italy@gmail.com>
2021-06-05 12:51:48 +0200

    feat: Add mutex err guard on ts_counter.c

--- src/server/ts_counter.c
+++ src/server/ts_counter.c
@@ -1,17 +1,31 @@
 #include "ts_counter.h"
+#include "global_state.h"
 #include <pthread.h>
+#include <stdlib.h>
 
 static pthread_mutex_t thread_id_guard = PTHREAD_MUTEX_INITIALIZER;
 static unsigned thread_id_counter = 0;
 
+static void
+on_mutex_err(void)
+{
+	glog_fatal("Unexpected mutex error. This is a bug and should be fixed.");
+	abort();
+}
+
 unsigned
 ts_counter(void)
 {
-	// TODO: error checking.
 	int err = 0;
 	err = pthread_mutex_lock(&thread_id_guard);
+	if (err) {
+		on_mutex_err();
+	}
 	unsigned id = thread_id_counter;
 	thread_id_counter++;
 	err = pthread_mutex_unlock(&thread_id_guard);
+	if (err) {
+		on_mutex_err();
+	}
 	return id;
 }
