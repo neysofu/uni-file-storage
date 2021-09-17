efd8b3d0001ef3c2b268c6a348e01228dab8926e
Filippo Costa <filippocosta.italy@gmail.com>
2021-09-18 01:05:32 +0200

    Remove outdated test files, improve test1.sh

--- test/test1.sh
+++ test/test1.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-# Prints the help message.
+PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+echo "The parent path of this test is $PARENT_PATH."
+
 ./client -h
-# Stores all files found in `test/data`.
-./client -p -f /tmp/LSOfiletorage.sk -Z 200 -w test/data/text
-# Read back all files.
-./client -p -f /tmp/LSOfiletorage.sk -Z 200 -R -d test/data
+
+for file in "$PARENT_PATH/data/Imgur/80s/*"; do
+   ./client -p -f /tmp/LSOfiletorage.sk -w $file -z 1
+done
+
+for file in "$PARENT_PATH/data/Imgur/80s/*"; do
+   ./client -p -f /tmp/LSOfiletorage.sk -r $file -d $PARENT_PATH/data/target/ -z 1
+done
+
+find "data/target/80s" -type f -exec md5sum {} + | sort -k 2 > $PARENT_PATH/data/target/md5.txt
+find "data/Imgur/80s" -type f -exec md5sum {} + | sort -k 2 > $PARENT_PATH/data/target/original.txt
+
+# Compare the original directory and the one we got from the server's contents.
+diff -u $PARENT_PATH/data/target/md5.txt $PARENT_PATH/data/target/original.txt
 
 exit 0
