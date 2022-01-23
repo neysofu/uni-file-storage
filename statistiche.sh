899a3726adf9b1da9d0a3049fa1dd63ab67247be
Filippo Costa <filippocosta.italy@gmail.com>
2022-01-23 18:03:44 +0100

    Improve docs

--- statistiche.sh
+++ statistiche.sh
@@ -14,14 +14,25 @@ NUM_WRITES=`grep -r "New API request \\\`writeFile\\\`" $1 | wc -l`
 NUM_OPENS=`grep -r "New API request \\\`openFile\\\`, create = 1, lock = 0." $1 | wc -l`
 NUM_CLOSES=`grep -r "New API request \\\`closeFile\\\`" $1 | wc -l`
 NUM_BYTES_READ='0'
+NUM_OPERATIONS_BY_WORKER_ID=(0)
 NUM_BYTES_WRITTEN='0'
 MAX_NUM_CONCURRENT_CONNECTIONS='0'
 NUM_CONNECTIONS=`grep -r "Adding a new connection" $1 | wc -l`
 NUM_EVICTIONS='0'
 NUM_EVICTED_FILES='0'
+NUM_WORKERS='0'
 
 while read LINE; do
-	if [[ $LINE == *"This read operation consists of"* ]]; then
+	if [[ $LINE == *"The latest message is"* ]]; then
+		WORKER_ID=`grep -oP '(?<=n\.)[0-9]+(?=\])' <<< $LINE`
+		# FIXME
+		if [ "${WORKER_ID}" -ge "${NUM_WORKERS}" ]; then
+			NUM_OPERATIONS_BY_WORKER_ID+=(1)
+			let NUM_WORKERS+=1
+		else
+			NUM_OPERATIONS_BY_WORKER_ID[$WORKER_ID]=$(( $WORKER_ID+1 ))
+		fi
+	elif [[ $LINE == *"This read operation consists of"* ]]; then
 		NUM_BYTES=`grep -oP '(?<=consists of )[0-9]+(?= bytes)' <<< $LINE`
 		NUM_BYTES_READ=$[$NUM_BYTES_READ + $NUM_BYTES]
 	elif [[ $LINE == *"This write operation consists of"* ]]; then
@@ -39,8 +50,13 @@ while read LINE; do
 	fi
 done < "$LOG_FILE"
 
-NUM_BYTES_READ=$[$NUM_BYTES_READ / $NUM_READS]
-NUM_BYTES_WRITTEN=$[$NUM_BYTES_WRITTEN / $NUM_WRITES]
+if [ $NUM_READS -ne "0" ]; then
+	NUM_BYTES_READ=$[$NUM_BYTES_READ / $NUM_READS]
+fi
+
+if [ $NUM_WRITES -ne "0" ]; then
+	NUM_BYTES_WRITTEN=$[$NUM_BYTES_WRITTEN / $NUM_WRITES]
+fi
 
 echo "Num. of new connections: $NUM_CONNECTIONS"
 echo "Num. of read operations: $NUM_READS"
@@ -55,5 +71,9 @@ echo "Num. of evicted files: $NUM_EVICTED_FILES"
 echo "Max. number of concurrent connections: $MAX_NUM_CONCURRENT_CONNECTIONS"
 echo "Avg. size of read operations in bytes: $NUM_BYTES_READ"
 echo "Avg. size of write operations in bytes: $NUM_BYTES_WRITTEN"
+
+for (( i=0; i<${NUM_WORKERS}; i++ )); do
+	echo "Num. of operations handled by worker n.$i: ${NUM_OPERATIONS_BY_WORKER_ID[$i]}"
+done
 
 exit 0
